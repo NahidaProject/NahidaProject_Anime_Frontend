a251ef645beeaa6d7eeb08abf310bc06dd4d976e
baizhi958216 <1475289190@qq.com>
2022-11-10 00:19:30 +0800

    Anime: Create index router
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import router from './router/router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/router/router.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import Anime from '../pages/Anime/index.vue'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        component: () => Anime,
+        meta: {
+            title: '欢迎来到Nahida Anime'
+        }
+    }
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory()
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to.meta.title){
+        document.title = to.meta.title as string
+    }
+})
+
+export default router
