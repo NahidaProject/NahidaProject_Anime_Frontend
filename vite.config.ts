693f394320c8452db852b7e4860288b1013ee076
baizhi958216 <1475289190@qq.com>
2022-11-10 23:53:28 +0800

    Anime: Fix CORS
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server:{
+    proxy:{
+      '/getip':{
+        target:'http://pv.sohu.com/cityjson?ie=utf-8',
+        changeOrigin:true
+      }
+    }
+  }
 })
