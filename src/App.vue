44e9320de4451a795d3f5944c2d5b36eeb148420
baizhi958216 <1475289190@qq.com>
2022-12-10 11:54:20 +0800

    Anime: Add gsap * GreenSock
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
-  </div>
+  <transition appear @before-enter="beforeEnter" @enter="enter">
+    <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button class="btn btn-primary">Primary button</button>
+    </div>
+  </transition>
 </template>
 
 <script setup>
-
+import gsap from 'gsap'
+const beforeEnter = (el) => {
+  el.childNodes[0].style.opacity = '0'
+  el.childNodes[0].style.transform = 'translateY(-100px)'
+}
+// where the animation will end up
+const enter = (el) => {
+  gsap.to(el.childNodes[0], {
+    duration: 1,
+    y: 0,
+    opacity: 1,
+  })
+}
 </script>
 
 <style lang="scss" scoped>
