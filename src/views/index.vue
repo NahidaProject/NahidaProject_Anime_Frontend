cc8ba1d20db918adc7c6509bb284af8f1552eecf
baizhi958216 <1475289190@qq.com>
2022-12-11 20:12:58 +0800

    Index: Import YiYan
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
-      <Navigation page="/"></Navigation>
-      <Footer></Footer>
+  <div class="container-fluid">
+    <Navigation page="/"></Navigation>
+    <div class="content">
+      <YiYan />
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup lang="ts">
 import Navigation from '../components/Navigation/index.vue'
 import Footer from '../components/Footer/index.vue'
+import YiYan from '../components/YiYan/index.vue'
+
 </script>
 <style scoped>
 
