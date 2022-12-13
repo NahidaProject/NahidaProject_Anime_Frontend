af0c1cd7ab7eb37af7086ab5c7fa9fdfd7d4016b
baizhi958216 <1475289190@qq.com>
2022-12-13 14:26:49 +0800

    Footer: Less padding and remove margin
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 border-top">
         <p class="col-md-4 mb-0 text-muted">© 2022 Nahida Project, Inc</p>
 
         <a href="/"
