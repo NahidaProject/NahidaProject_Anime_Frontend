863bd9f0d4447f773a5fced806ed6eaece2788d6
baizhi958216 <1475289190@qq.com>
2022-12-13 18:32:06 +0800

    animes: Add carousel
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/components/Carousel/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container-fluid">
+        <div id="carousel" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div :class="index == 0 ? 'carousel-item active data-bs-interval' : 'carousel-item  data-bs-interval'"
+                    v-for="item,index in newsList">
+                    <div class="card-img d-block w-100"
+                        :style="{ 'background-image': 'url(http://localhost:1314/anime/main_image/' + `${item.AnimeID >= 10 ? '0000' + item.AnimeID : '00000' + item.AnimeID}` + '.png)' }">
+                    </div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1>{{ item.AnimeName }}</h1>
+                        <p class="text-end">{{ item.AnimeStats }}...</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+let newsList: any = reactive([])
+
+fetch('http://localhost:1314/api/anime/GetRecommandAnimes').then(async res => {
+    newsList.push(...await res.json())
+})
+
+</script>
+
+<style scoped>
+.carousel-caption {
+    font-family: deyihei;
+    height: 35%;
+}
+
+.card-img {
+    background-size: cover;
+    background-position: center;
+    height: 30vh;
+    border-radius: 30px;
+}
+</style>
--- src/views/animes.vue
+++ src/views/animes.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container-fluid">
         <Navigation page="animes"></Navigation>
+        <div class="content">
+            <Carousel></Carousel>
+        </div>
         <Footer></Footer>
     </div>
 </template>
@@ -8,6 +11,8 @@
 <script setup lang="ts">
 import Navigation from '../components/Navigation/index.vue'
 import Footer from '../components/Footer/index.vue'
+import Carousel from '../components/Carousel/index.vue'
+
 </script>
 
 <style scoped>
