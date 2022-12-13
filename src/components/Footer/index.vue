938a6af61022599b970c863da74040d0b481a585
baizhi958216 <1475289190@qq.com>
2022-12-13 18:34:23 +0800

    Footer: Remove index href
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -2,13 +2,6 @@
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 border-top">
         <p class="col-md-4 mb-0 text-muted">© 2022 Nahida Project, Inc</p>
 
-        <a href="/"
-            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-            </svg>
-        </a>
-
         <ul class="nav col-md-4 justify-content-end">
             <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
             <li class="nav-item"><a href="https://www.gitee.com/nahida-project" class="nav-link px-2 text-muted">Gitee</a></li>
